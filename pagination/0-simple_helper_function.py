8e8369b57dbd38ca2a85ae96c6a476948e5d47ea
CodexSC <carlos.alexandur@gmail.com>
2026-04-20 11:20:02 +0200

    Synthax corrections

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -11,5 +11,5 @@ def index_range(page, page_size):
     particular pagination parameters.
     """
     start_index = (page - 1) * page_size
-    end_index = page * page_size
-    return (start_index, end_index)
+    end_index = start_index + page_size
+    return start_index, end_index
