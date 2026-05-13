07f69293e75f21a5862a888afd3122b785738779
CodexSC <carlos.alexandur@gmail.com>
2026-05-13 14:52:25 +0200

    Refactor console message formatting and fix typo in stdin.js; add countStudents function in read_file.js

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
-function displayMessage (message) {
-    console.log (message);
+function displayMessage(message) {
+    console.log(message);
 }
 
-module.exports = displayMessage
+module.exports = displayMessage;
 
