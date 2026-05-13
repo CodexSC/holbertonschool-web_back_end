d85e92002d5d3ca553fd6ba22109369dd6dbda8d
CodexSC <carlos.alexandur@gmail.com>
2026-05-13 14:21:33 +0200

    Implemented Node exercises 0-2

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,7 @@
+process.stout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+});
+process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+});
